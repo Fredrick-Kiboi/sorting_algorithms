53a3eba490125a5a8c26274ffaf7a453456e7abf
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-06-27 16:00:11 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,20 +10,20 @@
  */
 
 void bubble_sort(int * array, size_t size) {
-  size_t i, j;
-  int temp;
+    size_t i, j;
+    int temp;
 
-  for (i = 0; i < size - 1; i++) {
-    for (j = 0; j < size - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        /* Swap two elements */
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                /* Swap two elements */
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
 
-        /* Print the array after each swap */
-        print_array(array, size);
-      }
+                /* Print the array after each swap */
+                print_array(array, size);
+            }
+        }
     }
-  }
 }
